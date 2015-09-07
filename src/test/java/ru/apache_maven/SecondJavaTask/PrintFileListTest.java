3f474d90308e525119b8884711f60698708c2444
Ramul2B <ramul2b@mail.ru>
2015-09-07 04:21:53 +0300

    Test and comments are added. Last commit.

--- src/test/java/ru/apache_maven/SecondJavaTask/PrintFileListTest.java
+++ src/test/java/ru/apache_maven/SecondJavaTask/PrintFileListTest.java
@@ -1,38 +1,44 @@
 package ru.apache_maven.SecondJavaTask;
 
+import java.io.File;
+
+import ru.apache_maven.SecondJavaTask.PrintFileListTest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.textui.TestRunner;
 
-/**
- * Unit test for simple App.
- */
-public class PrintFileListTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public PrintFileListTest( String testName )
-    {
+public class PrintFileListTest extends TestCase{
+
+  public PrintFileListTest( String testName ){
         super( testName );
-    }
+  }
+ 
+  public void testOfPrintList(){
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( PrintFileListTest.class );
-    }
+/** Setting user directory. */
+    String userDirectory = System.getProperty("user.dir");
+    File directory = new File(userDirectory);
+    
+    int vsego = 109;
+    
+    int result = PrintFileList.createArray(directory).size();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+/** Assessed number of files and directories in the user directory. */
+    assertTrue( vsego != result);
+    
+  }
+  
+  public static Test suite(){
+    return new TestSuite( PrintFileListTest.class );
+  }
+
+  public void testApp(){
+      
+    TestRunner runner = new TestRunner();
+    TestSuite suite = new TestSuite();
+  
+    suite.addTest(new PrintFileListTest("testOfPrintList"));
+    runner.doRun(suite);
+  }
 }
